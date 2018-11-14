79f8fb6ff6418cb3c9ed67ffeb756acf3abf6113
diwao <nakapom@gmail.com>
2018-11-15 00:01:05 +0900

    feat: 全体的に構成を見直し

--- gulp/copy.js
+++ gulp/copy.js
@@ -7,12 +7,12 @@ const conf = require('../config');
 gulp.task('copy', (done) => {
   const dest = conf.dest;
   // image
-  gulp.src('./app/src/images/**')
-    .pipe(gulp.dest(dest + '/assets/images')
-  );
+  // gulp.src('./app/src/images/**')
+  //   .pipe(gulp.dest(dest + '/assets/images')
+  // );
   // その他
-  gulp.src('./app/src/other/**')
-    .pipe(gulp.dest(dest + '/')
-  );
+  // gulp.src('./app/src/other/**')
+  //   .pipe(gulp.dest(dest + '/')
+  // );
   done();
 });
