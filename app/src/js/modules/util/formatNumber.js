2c6e39c08dbeb982456f42ad47e22da122e07bdc
iwao <iwao@sria.co.jp>
2019-09-28 00:31:26 +0900

    srcのjsを削除

--- app/src/js/modules/util/formatNumber.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-export default (value) => {
-  if (typeof value !== 'number') {
-    return false;
-  }
-  const arr = value.toString().split('.');
-  arr[0] = arr[0].toString().replace(/(\d)(?=(\d{3})+$)/g, '$1,');
-  return arr.join('.');
-}
--- app/src/js/modules/util/zeroPadding.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-export default (num) => {
-  const str = '0' + num;
-  return str.substr(-2, 2);
-}
