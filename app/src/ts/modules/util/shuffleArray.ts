a5a7b7c8e907192fc980cc838aa761e9f3824751
iwao <iwao@sria.co.jp>
2019-09-28 20:35:29 +0900

    shuffleArrayを調整

--- app/src/ts/modules/util/shuffleArray.ts
+++ app/src/ts/modules/util/shuffleArray.ts
@@ -1,12 +1,10 @@
 'use strict';
 
-export default (array) => {
-  let n = array.length;
-  let t;
-  let i;
+export default (array: any[]): any[] => {
+  let n: number = array.length;
   while(n) {
-    i = Math.floor(Math.random() * n--);
-    t = array[n];
+    let i: number = Math.floor(Math.random() * n--);
+    let t = array[n];
     array[n] = array[i];
     array[i] = t;
   }
