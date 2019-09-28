272115e5bc086b644282228f585caaaeff3c8bda
iwao <iwao@sria.co.jp>
2019-09-28 22:33:03 +0900

    調整

--- app/src/ts/modules/util/parallaxTranslate.ts
+++ app/src/ts/modules/util/parallaxTranslate.ts
@@ -1,13 +1,15 @@
 'use strict';
 
+import * as $ from 'jquery';
+
 // st: scrollTop
-export default (st, elm) => {
-  const $elm = $(elm);
-  const ratio = $elm.attr('data-ratio');
-  const moveVal = (st - $elm.offset().top - $elm.height() + $(window).height()) * Number(ratio);
+export default (st: number, el: string) => {
+  const $el = $(el);
+  const ratio = $el.attr('data-ratio');
+  const moveVal = (st - $el.offset().top - $el.height() + $(window).height()) * Number(ratio);
   // console.log(moveVal);
   if (moveVal > 0) {
-    $elm.css({
+    $el.css({
       transform: `translateY(${moveVal}px)`
     });
   }
