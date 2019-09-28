63b72f2ada27d65c531f3e9758b83fc38d3c7aeb
iwao <iwao@sria.co.jp>
2019-09-28 15:42:52 +0900

    zeroPaddingを修正

--- app/src/ts/modules/util/zeroPadding.ts
+++ app/src/ts/modules/util/zeroPadding.ts
@@ -1,6 +1,13 @@
 'use strict';
 
-export default (num: number): string => {
-  const str = '0' + num;
-  return str.substr(-2, 2);
+// ゼロ埋めした二桁の値を文字列で返却する
+export default (_val: number): string => {
+  let ret: string;
+  const val: number = Math.floor(_val);
+  const str = '0' + val;
+  ret = str.substr(-2, 2);
+  if (val > 99) {
+    ret = String(val);
+  }
+  return ret;
 };
